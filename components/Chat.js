e63c8007b2c99aac4996e7c7d3c8aa2c387e425a
Micah Glausier <micah.glausier@me.com>
2023-10-16 11:52:53 -0500

    Implemented Chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,71 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+
+
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;  //Brings name and bg color selected to Chat
+  const [messages, setMessages] = useState([]);
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
 
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
- return (
-  <View style={[styles.container, { backgroundColor: color }]}>
-    <Text>START YOUR CHAT!</Text>
-  </View>
- );
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello there!',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'Welcome to the Chat!',
+        createdAt: new Date(),
+        system: true,
+      },
+
+    ]);
+  }, []);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: color }}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+    {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+    {Platform.OS === 'ios' ? <KeyboardAvoidingView behavior="padding" /> : null }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
